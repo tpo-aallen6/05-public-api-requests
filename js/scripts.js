e7091fd0fc5bc314a38593c0feef55f74cb781c4
Allen <AALLEN6@travelers.com>
2021-02-12 09:06:16 -0500

    add functions
    fully meets expectations

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,80 @@
-function createCard (person) {
-  const card = `
-        <div class="card">
-          <div class="card-img-container">
-            <img class="card-img" src="${person.picture.large}" alt="profile picture">
-          </div>
-          <div class="card-info-container">
-            <h3 class="card-name cap">${person.name.first} ${person.name.last}</h3>
-            <p class="card-text">${person.email}</p>
-            <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
-          </div>
-        </div>`
-  return card
-}
+document.querySelector('body').style.backgroundColor = 'LightSteelBlue'
+document.querySelector('h1').style.color = 'black'
 
-function createModal (card) {
-  // do something
-}
+function createCard (data) {
+  data.results.forEach(result => {
+    const card = document.createElement('div')
+    const cardText = Array.from(document.querySelectorAll('.card-text'))
+    const fullName = `${result.name.first} ${result.name.last}`
 
-fetch('https://randomuser.me/api/?results=12&nat=us')
-  .then(data => data.json())
-  .then(results => {
-    const people = results.results
-    let cardsHTML = ''
-    for (let i = 0; i < people.length; i++) {
-      cardsHTML += createCard(people[i])
-    }
-    return cardsHTML
+    const html = `
+      <div class="card-img-container">
+        <img class="card-img" src="${result.picture.large}" alt="profile picture">
+      </div>
+      <div class="card-info-container">
+        <h3 id="name" class="card-name" cap">${fullName}</h3>
+        <p class="card-text">${result.email}</p>
+        <p class="card-text" cap>${result.location.city}</p>
+      </div>`
+
+    card.className = 'card'
+    card.style.backgroundColor = 'OldLace'
+    card.style.borderWidth = 'thick'
+    cardText.forEach(card => { card.style.color = 'black' })
+    card.insertAdjacentHTML('beforeend', html)
+    document.querySelector('#gallery').insertAdjacentElement('beforeend', card)
+
+    card.addEventListener('click', () => createModal(result))
   })
-  .then(allCards => document.querySelector('#gallery')
-    .insertAdjacentHTML('beforeend', allCards))
+}
+
+function createModal (result) {
+  const modal = document.createElement('div')
+  const loc = result.location
+  const street = `${loc.street.number} ${loc.street.name}`
+  const fullName = `${result.name.first} ${result.name.last}`
+  const birthday = result.dob.date
+  const month = birthday.substr(5, 2)
+  const day = birthday.substr(8, 2)
+  const year = birthday.substr(2, 2)
+  const formattedBday = `${month}/${day}/${year}`
+
+  const html = `
+    <div class="modal">
+      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+      <div class="modal-info-container">
+        <img class="modal-img" src="${result.picture.large}" alt="profile picture">
+        <h3 id="name" class="modal-name cap">${fullName}</h3>
+        <p class="modal-text">${result.email}</p>
+        <p class="modal-text cap">${loc.city}</p>
+        <hr>
+        <p class="modal-text">${result.cell.replace(/-/, ' ')}</p>
+        <p class="modal-text">${street}, ${loc.state} ${loc.postcode}</p>
+        <p class="modal-text">Birthday: ${formattedBday}</p>
+      </div>
+    </div>
+    <div class="modal-btn-container">
+      <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+      <button type="button" id="modal-next" class="modal-next btn">Next</button>
+    </div>`
+
+  modal.className = 'modal-container'
+  modal.insertAdjacentHTML('beforeend', html)
+  document.body.insertAdjacentElement('beforeend', modal)
+
+  document.querySelector('#modal-close-btn').addEventListener('click', () => {
+    modal.remove()
+  })
+}
+
+fetch('https://randomuser.me/api?results=12&nat=us')
+  .then(response => { return response.json() })
+  .then(data => createCard(data))
+
+const searchBar = `
+  <form action="#" method="get">
+    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+  </form>`
+
+document.querySelector('.search-container').insertAdjacentHTML('beforeend', searchBar)
